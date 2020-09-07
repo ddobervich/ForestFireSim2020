b9b66320c06d2f378996a7fefdc983d72497206a
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2020-09-06 20:00:08 -0700

    Add solution code

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,4 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import processing.core.*;
 
 public class GUI extends PApplet {
@@ -9,24 +10,32 @@ public class GUI extends PApplet {
     }
 
     public void setup() {
+        frameRate(2);
+
         // Create a simulator
         sim = new Simulator(100, 100);
+        sim.initialize(0.37);
+        int count = sim.count(Simulator.LIVE_TREE);
+        System.out.println(count + "/" + sim.size() + " = " + ((double) count / sim.size()));
+
+        for (int i = 0; i < 5; i++) {
+            sim.setPointFire();
+        }
 
         // Create the display
         // parameters: (10,10) is upper left of display
         // (620, 530) is the width and height
         gridDisplay = new DisplayWindow(this, 10, 10, 620, 530);
 
-        gridDisplay.setNumCols(sim.getWidth());		// NOTE:  these must match your simulator!!
+        gridDisplay.setNumCols(sim.getWidth());        // NOTE:  these must match your simulator!!
         gridDisplay.setNumRows(sim.getHeight());
 
         // Set different grid values to different colors
-        // TODO:  uncomment these lines!
-        // display.setColor(Simulator.ON_FIRE, color(255, 0, 0));
-        // display.setColor(Simulator.ASH, color(180, 180, 180));
-        // display.setColor(Simulator.LIVING, color(0, 0, 255));
-        // display.setColor(Simulator.EMPTY, color(255, 255, 255));
 
+        gridDisplay.setColor(Simulator.FIRE, color(255, 0, 0));
+        gridDisplay.setColor(Simulator.ASH, color(180, 180, 180));
+        gridDisplay.setColor(Simulator.LIVE_TREE, color(0, 255, 0));
+        gridDisplay.setColor(Simulator.EMPTY, color(255, 255, 255));
     }
 
     @Override
@@ -34,6 +43,7 @@ public class GUI extends PApplet {
         background(200);
 
         // TODO: have your simulation run one step.
+        sim.runOneStep();
 
         gridDisplay.drawGrid(sim.getDisplayGrid()); // display the game
     }
